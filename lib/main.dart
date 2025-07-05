0f13fde35e6ccfd76336d9eff22ef7031587946a
Zaharaaz <153468491+Zaharaaz@users.noreply.github.com>
2025-07-05 20:41:06 +0700

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:app_malang/main_screen.dart';
 
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wisata Malang',
+      theme: ThemeData(),
+      home: const MainScreen(),
+    );
+  }
+}
